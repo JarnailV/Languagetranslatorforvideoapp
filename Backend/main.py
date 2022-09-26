36ac9b1bcac1949072613205a6a25851ae62df9c
JarnailV <114377145+JarnailV@users.noreply.github.com>
2022-09-26 14:47:57 +0500

    Add files via upload

--- Backend/main.py
+++ Backend/main.py
@@ -1,21 +1,33 @@
-
+# coding=utf8
 from gtts import gTTS
 import gradio as gr
 import os
 import speech_recognition as sr
 from googletrans import Translator, constants
 from pprint import pprint
+#pip install moviepy
+#pip3 install googletrans
 from moviepy.editor import *
 from Model import translate
 def video_to_translate(file_obj,initial_language,final_language):
+# Insert Local Video File Path
     videoclip = VideoFileClip(file_obj.name)
+    # Insert Local Audio File Path
     videoclip.audio.write_audiofile("test.wav",codec='pcm_s16le')
+# initialize the recognizer
     r = sr.Recognizer()
 
     
+
+
+
+    # open the file
     with sr.AudioFile("test.wav") as source:
+        # listen for the data (load audio to memory)
         audio_data = r.record(source)
+        # recognize (convert from speech to text)
         text = r.recognize_google(audio_data, language = "en-US")
+        print(text)
 
     
     if final_language == "French":
@@ -28,11 +40,14 @@ def video_to_translate(file_obj,initial_language,final_language):
     myobj = gTTS(text=trans, lang=lang, slow=True) 
     myobj.save("audio.wav") 
 
+    # loading audio file
     audioclip = AudioFileClip("audio.wav")
     
+    # adding audio to the video clip
     new_audioclip = CompositeAudioClip([audioclip])
     videoclip.audio = new_audioclip
     videoclip.write_videofile("new_filename.mp4")
+    #return 'audio.wav'
     return 'new_filename.mp4'
 
 
